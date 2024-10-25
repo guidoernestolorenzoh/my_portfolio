2f755a118ad3766cc37efda2ccb9f77ccb732498
Guido Ernesto <guidoernestolh@gmail.com>
2024-10-25 12:00:33 -0400

    second commit

--- pages/services/index.js
+++ pages/services/index.js
@@ -14,16 +14,36 @@ const Services = () => {
         <div className="flex flex-col xl:flex-row gap-x-8">
           {/* text */}
           <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
-            <h2 className="h2 xl:mt-8">
-              My services <span className="text-accent"></span>
-            </h2>
-            <p className="mb-4 max-w-[480px] mx-auto lg:mx-0">
+            <motion.h2 
+              className="h2 xl:mt-8"
+              variants={fadeIn('up', 0.2)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+            >
+              My services <span className="text-accent">.</span>
+            </motion.h2>
+            <motion.p 
+              className="mb-4 max-w-[400px] mx-auto lg:mx-0"
+              variants={fadeIn('up', 0.4)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+            >
               Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsum
               Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
-            </p>
+            </motion.p>
           </div>
-          {/* slider */}
-          <ServiceSlider />
+          <motion.div 
+            className="w-full xl:max-w-[65%]"
+            variants={fadeIn('down', 0.6)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+          >
+            {/* slider */}
+            <ServiceSlider />
+          </motion.div>
         </div>
       </div>
       <Bulb />
--- pages/work/index.js
+++ pages/work/index.js
@@ -1,5 +1,53 @@
-const Work = () => {
-  return <div>Work</div>;
-};
+import WorkSlider from "../../components/WorkSlider";
+import Bulb from "../../components/Bulb";
+import Circles from "../../components/Circles";
 
+import {motion} from "framer-motion";
+import {fadeIn} from "../../variants";
+
+const Work = () => {
+  return (
+    <div className="h-full bg-primary/30 py-36 flex items-center">
+      <Circles />
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-x-8">
+          {/* text */}
+          <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
+            <motion.h2 
+              className="h2 xl:mt-12"
+              variants={fadeIn('up', 0.2)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+            >
+              My work <span className="text-accent">.</span>
+            </motion.h2>
+            <motion.p 
+              className="mb-4 max-w-[400px] mx-auto lg:mx-0"
+              variants={fadeIn('up', 0.4)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+            >
+              Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsum
+              Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
+            </motion.p>
+          </div>
+          <motion.div 
+            className="w-full xl:max-w-[65%]"
+            variants={fadeIn('down', 0.6)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+          >
+            {/* slider */}
+            <WorkSlider />
+          </motion.div>
+        </div>
+      </div>
+      <Bulb />
+    </div>
+  );
+};
 export default Work;
+
